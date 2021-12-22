942016b740623c107b0be04a5d434309e70289f3
guuicarl <guilherme.carlos014@gmail.com>
2021-12-22 17:20:51 -0300

    colocando gif

--- src/action.js
+++ src/action.js
@@ -1,56 +1,27 @@
-// @ts-ignore
-const fetch = require("node-fetch");
-const core = require("@actions/core");
-const github = require("@actions/github");
+const fetch = require('node-fetch');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-async function run() {
-  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-  const TENOR_TOKEN = core.getInput("TENOR_TOKEN") || process.env.TENOR_TOKEN;
-  const message = core.getInput("message") || "Thank you!";
-  const searchTerm = core.getInput("searchTerm") || "thank you";
-
-  if (typeof TENOR_TOKEN !== "string") {
-    throw new Error(
-      "Invalid TENOR_TOKEN: did you forget to set it in your action config?"
-    );
-  }
-
-  if (typeof GITHUB_TOKEN !== "string") {
-    throw new Error(
-      "Invalid GITHUB_TOKEN: did you forget to set it in your action config?"
-    );
-  }
-
-  const randomPos = Math.round(Math.random() * 1000);
-  const url =
-  "https://api.tenor.com/v1/search?q=love&pos=$123&limit=1&media_filter=minimal&contentfilter=high&key=HS0UNSXONQCL";
+async function run(){
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
     
-  console.log(`Searching Tenor: ${url}`);
+    const randomPos = Math.round(Math.random() * 50);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+    const response = await fetch(url);
+    const { results } = await response.json();
+    const gifUrl = results[0].media[0].tinygif.url;
 
-  const response = await fetch(url);
-  const { results } = await response.json();
-  const gifUrl = results[0].media[0].tinygif.url;
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
-  console.log(`Found gif from Tenor: ${gifUrl}`);
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
 
-  const { context = {} } = github;
-  // @ts-ignore
-  const { pull_request } = context.payload;
-
-  if (!pull_request) {
-    throw new Error("Could not find pull request!");
-  }
-
-  console.log(`Found pull request: ${pull_request.number}`);
-
-  const octokit = github.getOctokit(GITHUB_TOKEN);
-
-  await octokit.issues.createComment({
-    // @ts-ignore
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: `${message}`,
-  });
+    await octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`
+      });
 }
 
-run().catch((e) => core.setFailed(e.message));
+run ();
